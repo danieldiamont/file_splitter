746492d94a0b6199f9eb9cf0fe9869ce10aed0c7
Daniel Diamont <daniel.diamont@gmail.com>
2024-06-19 15:13:02 -0500

    added comparison with another directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use num_integer::div_rem;
+use std::path::PathBuf;
 use std::{
     fs::File,
     io::{Read, Write},
@@ -11,20 +12,18 @@ const CRC_POLYNOMIAL: u32 = 0xEDB88320;
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    file_name: String,
+    file_path: PathBuf,
 
     #[arg(long, default_value_t = 2048)]
     chunk_size: usize,
 
-    #[arg(long, action)]
-    compute_crcs: bool,
-
     #[arg(long)]
-    compare_with: Option<String>,
+    compare_dir: Option<PathBuf>,
 }
 
-fn crc32_file(file_name: String) -> u32 {
-    let mut file = File::open(file_name).unwrap();
+fn crc32_file(file_path: PathBuf) -> u32 {
+    let mut file = File::open(file_path.clone())
+        .expect(format!("Tried to open {:#?}", file_path.clone()).as_str());
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     return crc32(&mut buffer);
@@ -47,14 +46,14 @@ fn crc32(buffer: &mut Vec<u8>) -> u32 {
 }
 
 fn get_part_name(
-    file_name: String,
+    file_path: String,
     part_num: usize,
     chunk_size: usize,
     file_size: usize,
 ) -> String {
     let num_chunks = calculate_num_chunks(file_size, chunk_size);
     let width = count_digits(num_chunks);
-    return format!("{}.{:0width$}", file_name, part_num, width = width);
+    return format!("{}.{:0width$}", file_path, part_num, width = width);
 }
 
 fn count_digits(num: usize) -> usize {
@@ -72,48 +71,71 @@ fn calculate_num_chunks(file_size: usize, chunk_size: usize) -> usize {
     return q + ((r > 0) as usize);
 }
 
-fn process_chunk(buf: &mut Vec<u8>, num_bytes: usize, file_name: String, compute_crcs: bool) {
-    let mut part_file = File::create(file_name.clone()).unwrap();
+fn process_chunk(
+    buf: &mut Vec<u8>,
+    num_bytes: usize,
+    file_path: PathBuf,
+    compare: Option<PathBuf>,
+) {
+    let mut part_file = File::create(file_path.clone()).unwrap();
     let _ = part_file.write_all(&buf[0..num_bytes]);
 
-    if compute_crcs {
-        let crc = crc32_file(file_name.clone());
-        println!(
-            "Processing chunk #: {}\t-- size: {} (bytes) -- crc32 = {}",
-            file_name.clone(),
-            num_bytes,
-            crc
-        );
-    } else {
-        println!(
-            "Processing chunk #: {}\t-- size: {} (bytes)",
-            file_name.clone(),
-            num_bytes
+    let crc = crc32_file(file_path.clone());
+    println!(
+        "Processing chunk #: {:#?}\t-- size: {} (bytes) -- crc32 = 0x{:08X}",
+        file_path.clone(),
+        num_bytes,
+        crc
+    );
+
+    let crc_comp: Option<u32> = compare.clone().map(|path| crc32_file(path));
+
+    if let Some(crc_comp_val) = crc_comp {
+        assert_eq!(
+            crc,
+            crc_comp_val,
+            "CRC Comparison {:#?} crc = 0x{:08X};\t{:#?} crc = 0x{:08X}",
+            file_path,
+            crc,
+            compare.unwrap(),
+            crc_comp_val
         );
     }
 }
 
-fn split_file(file_name: String, chunk_size: usize, compute_crcs: bool) {
+fn split_file(file_path: PathBuf, chunk_size: usize, compare_dir: Option<PathBuf>) {
     assert!(chunk_size > 0);
-    println!("filename: {}\t chunk_size: {}", file_name, chunk_size);
+    println!("filename: {:#?}\t chunk_size: {}", file_path, chunk_size);
 
-    let mut file = File::open(file_name.clone()).unwrap();
+    let mut file = File::open(file_path.clone()).unwrap();
     let mut buffer = vec![0; chunk_size];
     let mut _part_number = 0;
 
     let filesize = file.metadata().unwrap().len();
+    let filename = file_path.file_name().unwrap();
+    let parent_dir = file_path.parent().unwrap();
 
     loop {
         match file.read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
                 let part_file_name = get_part_name(
-                    file_name.clone(),
+                    filename.to_string_lossy().to_string(),
                     _part_number,
                     chunk_size,
                     filesize as usize,
                 );
-                process_chunk(&mut buffer, n, part_file_name, compute_crcs);
+
+                let mut part_file_path = PathBuf::new();
+                part_file_path.push(parent_dir);
+                part_file_path.push(part_file_name.clone());
+
+                let comp: Option<PathBuf> = compare_dir
+                    .as_ref()
+                    .map(|dir| dir.join(part_file_name.clone()));
+
+                process_chunk(&mut buffer, n, part_file_path, comp);
+
                 _part_number += 1;
             }
             Err(e) => panic!("ERROR: {:?}", e),
@@ -123,7 +145,7 @@ fn split_file(file_name: String, chunk_size: usize, compute_crcs: bool) {
 
 fn main() {
     let args = Args::parse();
-    split_file(args.file_name, args.chunk_size, args.compute_crcs);
+    split_file(args.file_path, args.chunk_size, args.compare_dir);
 }
 
 #[cfg(test)]
