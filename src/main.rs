1e53c75ca9216b23eaf8bda27db10d4970907ca4
Daniel Diamont <daniel.diamont@gmail.com>
2024-06-19 11:30:47 -0500

    crc32 unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::{fs::File, io::{Read, Write}};
 use clap::Parser;
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
-const CRC_POLYNOMIAL : u32 = 0x04C11DB7;
+const CRC_POLYNOMIAL: u32 = 0xEDB88320;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-
     #[arg(short, long)]
     file_name: String,
 
@@ -17,45 +19,58 @@ struct Args {
     compute_crcs: bool,
 
     #[arg(long)]
-    compare_with: Option<String>
-
+    compare_with: Option<String>,
 }
 
-fn crc32(file_name: String, num_bytes: usize) -> u32 {
-    let mut crc : u32 = 0xFFFFFFFF;
-
+fn crc32_file(file_name: String) -> u32 {
     let mut file = File::open(file_name).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
+    return crc32(&mut buffer);
+}
 
-    for i in 0..num_bytes {
-        let ch : u32 = buffer[i].into(); 
+fn crc32(buffer: &mut Vec<u8>) -> u32 {
+    let mut crc: u32 = 0xFFFFFFFF;
+
+    for &byte in buffer.iter() {
+        crc ^= byte as u32;
         for _ in 0..8 {
-            let tmp = (ch ^ crc) & 1;
-            crc >>= 1;
-            if tmp != 0 {
-                crc ^= CRC_POLYNOMIAL;
+            if crc & 1 == 1 {
+                crc = (crc >> 1) ^ CRC_POLYNOMIAL;
+            } else {
+                crc >>= 1;
             }
-            crc >>= 1;
         }
     }
-    return !crc;
+    return crc ^ 0xFFFFFFFF;
 }
 
 fn get_part_name(file_name: String, part_num: usize) -> String {
     return format!("{}.{:03}", file_name, part_num);
 }
 
-fn process_chunk(buf: &mut Vec<u8>, num_bytes: usize, file_name: String, part_num: usize, compute_crcs: bool) {
+fn process_chunk(
+    buf: &mut Vec<u8>,
+    num_bytes: usize,
+    file_name: String,
+    part_num: usize,
+    compute_crcs: bool,
+) {
     let part_file_name = get_part_name(file_name, part_num);
     let mut part_file = File::create(part_file_name.clone()).unwrap();
     let _ = part_file.write_all(&buf[0..num_bytes]);
 
     if compute_crcs {
-        let crc = crc32(part_file_name.clone(), num_bytes);
-        println!("Processing chunk #: {}\t-- size: {} (bytes) -- crc32 = {}", part_num, num_bytes, crc);
+        let crc = crc32_file(part_file_name.clone());
+        println!(
+            "Processing chunk #: {}\t-- size: {} (bytes) -- crc32 = {}",
+            part_num, num_bytes, crc
+        );
     } else {
-        println!("Processing chunk #: {}\t-- size: {} (bytes)", part_num, num_bytes);
+        println!(
+            "Processing chunk #: {}\t-- size: {} (bytes)",
+            part_num, num_bytes
+        );
     }
 }
 
@@ -71,9 +86,15 @@ fn split_file(file_name: String, chunk_size: usize, compute_crcs: bool) {
         match file.read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
-                process_chunk(&mut buffer, n, file_name.clone(), _part_number, compute_crcs);
+                process_chunk(
+                    &mut buffer,
+                    n,
+                    file_name.clone(),
+                    _part_number,
+                    compute_crcs,
+                );
                 _part_number += 1;
-            },
+            }
             Err(e) => panic!("ERROR: {:?}", e),
         }
     }
@@ -82,4 +103,31 @@ fn split_file(file_name: String, chunk_size: usize, compute_crcs: bool) {
 fn main() {
     let args = Args::parse();
     split_file(args.file_name, args.chunk_size, args.compute_crcs);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn crc32_empty_buffer() {
+        let mut buf: Vec<u8> = Vec::new();
+
+        assert_eq!(crc32(&mut buf), 0);
+    }
+
+    #[test]
+    fn crc32_single_byte_buffer() {
+        let mut buf: Vec<u8> = Vec::new();
+        buf.push(b'a');
+
+        assert_eq!(crc32(&mut buf), 3904355907);
+    }
+
+    #[test]
+    fn crc32_known_string() {
+        let mut buf: Vec<u8> = Vec::from("123456789".as_bytes());
+
+        assert_eq!(crc32(&mut buf), 3421780262);
+    }
 }
