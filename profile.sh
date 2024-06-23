bb48763ddf6d9a78b3d8fda02f195c78dbd7c198
Daniel Diamont <daniel.diamont@gmail.com>
2024-06-23 17:21:41 -0500

    profile script updates

--- profile.sh
+++ profile.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+FILE="Cargo.toml"
+DEBUG=$(grep "profile.release" Cargo.toml | wc -l)
+if [ $DEBUG -eq 0 ]; then
+    echo "" >> $FILE
+    echo "[profile.release]" >> $FILE
+    echo "debug = true" >> $FILE
+fi
+
+rm -f callgrind.out.*
 cargo build --release
 valgrind --tool=callgrind --collect-jumps=yes --dump-instr=yes ./target/release/file_splitter --file-path assets/large.txt --chunk-size 204800 --compare-dir tmp/
 ls -lht callgrind.out.* | head -n 1 | kcachegrind
